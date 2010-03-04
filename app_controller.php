48c3be7f7a8e3b86f263e0d1afdec41091070ffe
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-03-03 20:19:25 -0500

    setting up all components

--- app_controller.php
+++ app_controller.php
@@ -1,6 +1,25 @@
 <?php
 class AppController extends Controller {
-	var $components = array('DebugKit.Toolbar', 'Session');
+	var $components = array(
+		'Authsome.Authsome' => array('model' => 'Maintainer'),
+		'DebugKit.Toolbar',
+		'Session',
+		'SwiftMailer'
+	);
 	var $helpers = array('Form', 'Html', 'Resource', 'Session');
+
+	function __mailSetup($to, $subject) {
+		$this->SwiftMailer->smtpType = 'tls';
+		$this->SwiftMailer->smtpHost = 'smtp.gmail.com';
+		$this->SwiftMailer->smtpPort = 465;
+		$this->SwiftMailer->smtpUsername = Configure::read('Settings.SmtpUsername');
+		$this->SwiftMailer->smtpPassword = Configure::read('Settings.SmtpPassword');
+		$this->SwiftMailer->sendAs = 'html';
+		$this->SwiftMailer->from = Configure::read('Settings.ServerEmail');
+		$this->SwiftMailer->fromName = Configure::read('Settings.SiteTitle');
+		$this->SwiftMailer->replyTo = Configure::read('Settings.ServerEmail');
+		$this->SwiftMailer->to = $to;
+		$this->SwiftMailer->subject = $subject;
+	}
 }
 ?>
