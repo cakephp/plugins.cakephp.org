9b5e62cca3779dbaa680f0bb84b6f9cc4b5d14d7
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-03-03 20:32:40 -0500

    fixed improper call to Controller::__mailSetup()

--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -52,7 +52,7 @@ class UsersController extends AppController {
 			}
 
 			$activationKey = $this->Maintainer->changeActivationKey($maintainer['Maintainer']['id']);
-			$this->Swiftmailer->mailSetup($maintainer['Maintainer']['email'], '[CakePackages] ' . __('Reset Password', true));
+			$this->__mailSetup($maintainer['Maintainer']['email'], '[CakePackages] ' . __('Reset Password', true));
 			$this->set(compact('maintainer', 'activationKey'));
 			try { 
 				if(!$this->SwiftMailer->send('forgot_password', '[CakePackages] ' . __('Reset Password', true))) { 
