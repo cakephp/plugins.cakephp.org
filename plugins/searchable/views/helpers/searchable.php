f325672c0399d4ac778a2ad753a5a8cfd3ab4d56
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-04-02 22:29:54 -0300

    Spacing out SeachableHelper::snippets() data summaries

--- plugins/searchable/views/helpers/searchable.php
+++ plugins/searchable/views/helpers/searchable.php
@@ -7,7 +7,7 @@ class SearchableHelper extends AppHelper {
 		$term = (isset($this->data['SearchIndex']['term'])) ? trim($this->data['SearchIndex']['term']) : '';
 		$snippets = '';
 		while (strlen($snippets) < 255 && $value = next($data)) {
-			$snippets .= $this->Text->highlight($this->Text->excerpt($value, $term, 20), $term);
+			$snippets .= ' ' . $this->Text->highlight($this->Text->excerpt($value, $term, 20), $term);
 		}
 		return $snippets;
 	}
