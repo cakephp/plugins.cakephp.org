a06fb06f9ff2e3b19953c832edd4088d3ec935dc
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-04-16 01:06:20 -0400

    setting the path for the PermitComponent updates

--- app_controller.php
+++ app_controller.php
@@ -3,7 +3,7 @@ class AppController extends Controller {
 	var $components = array(
 		'Authsome.Authsome' => array('model' => 'Maintainer'),
 		'Mail',
-		'Sanction.Permit',
+		'Sanction.Permit' => array('path' => 'Maintainer.Maintainer'),
 		'Session',
 		'Settings.Settings',
 		'DebugKit.Toolbar',
