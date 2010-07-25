5d2224dc8bee064886d0cc3e46a8aa801b951eb0
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-07-25 13:27:57 -0400

    update to AppModel and Callbackable to remove lingering bugs

--- app_model.php
+++ app_model.php
@@ -1,22 +1,59 @@
 <?php
+/**
+ * Application Model class
+ *
+ * Add your application-wide methods to the class, your models will inherit them.
+ *
+ * @package       app
+ */
 class AppModel extends Model {
-	var $actsAs = array('Callbackable', 'Containable', 'Lookupable');
+/**
+ * List of behaviors to load when the model object is initialized. Settings can be
+ * passed to behaviors by using the behavior name as index. Eg:
+ *
+ * var $actsAs = array('Translate', 'MyBehavior' => array('setting1' => 'value1'))
+ *
+ * @var array
+ * @access public
+ * @link http://book.cakephp.org/view/1072/Using-Behaviors
+ */
+	var $actsAs = array(
+		'Callbackable',
+		'Containable',
+		'Log.Logable' => array('change' => 'full')
+	);
+
+/**
+ * Number of associations to recurse through during find calls. Fetches only
+ * the first level by default.
+ *
+ * @var integer
+ * @access public
+ * @link http://book.cakephp.org/view/1057/Model-Attributes#recursive-1063
+ */
 	var $recursive = -1;
-	var $behaviorData = null;
+
+/**
+ * Query currently executing.
+ *
+ * @var array
+ * @access public
+ */
 	var $query = null;
 
 /**
  * Custom find types, as per Matt Curry's method
  *
- * @param   string $type
- * @param   array $options
- * @return  mixed array|integer|boolean
- * @access  public
- * @author  Matt Curry
- * @link    http://github.com/mcurry/find
+ * @param string $type
+ * @param array $options
+ * @return mixed array|integer|boolean
+ * @access public
+ * @author Matt Curry
+ * @link http://github.com/mcurry/find
  */
 	function find($type, $options = array()) {
 		$method = null;
+		$options = (array) $options;
 		if (is_string($type)) {
 			$method = sprintf('__find%s', Inflector::camelize($type));
 		}
@@ -36,7 +73,7 @@ class AppModel extends Model {
 			unset($options['blacklist']);
 		}
 		if (!empty($options['cache'])) {
-			App::import('Vendor', 'mi_cache');
+			if (!class_exists('MiCache')) App::import('Vendor', 'mi_cache');
 			if (is_int($options['cache'])) MiCache::config(array('duration' => $options['cache']));
 			unset($options['cache']);
 			return MiCache::data($this->alias, 'find', $type, $options);
@@ -54,10 +91,12 @@ class AppModel extends Model {
 /**
  * Allows the returning of query parameters for use in pagination
  *
- * @param   array $query
- * @return  boolean
- * @access  public
- * @author  Matt Curry
+ * @param array $queryData Data used to execute this query, i.e. conditions, order, etc.
+ * @return mixed true if the operation should continue, false if it should abort; or, modified
+ *               $queryData to continue with new $queryData
+ * @access public
+ * @author Matt Curry
+ * @link http://book.cakephp.org/view/1048/Callback-Methods#beforeFind-1049
  */
 	function beforeFind($query = array()) {
 		$query = (array) $query;
@@ -81,17 +120,19 @@ class AppModel extends Model {
 	}
 
 /**
- * undocumented function
+ * Saves model data (based on white-list, if supplied) to the database. By
+ * default, validation occurs before save.
  *
- * @param   array $data Data to save.
- * @param   mixed $validate Either a boolean, or an array.
+ * @param array $data Data to save.
+ * @param mixed $validate Either a boolean, or an array.
  *   If a boolean, indicates whether or not to validate before saving.
  *   If an array, allows control of validate, callbacks, and fieldList
- * @param   array $fieldList List of fields to allow to be written
- * @param   array $extra controls access to optional data a Behavior may want
- * @return  mixed On success Model::$data if its not empty or true, false on failure
- * @access  public
- * @author  Jose Diaz-Gonzalez
+ * @param array $fieldList List of fields to allow to be written
+ * @param array $extra controls access to optional data a Behavior may want
+ * @return mixed On success Model::$data if its not empty or true, false on failure
+ * @access public
+ * @author Jose Diaz-Gonzalez
+ * @link http://book.cakephp.org/view/1031/Saving-Your-Data
  **/
 	function save($data = null, $validate = true, $fieldList = array(), $extra = array()) {
 		$this->data = (!$data) ? $this->data : $data;
@@ -100,7 +141,7 @@ class AppModel extends Model {
 		$options = array('validate' => true, 'fieldList' => array(), 'callbacks' => true);
 		if (is_array($validate)) {
 			$options = array_merge($options, $validate);
-			foreach ($options as $key => &$value) {
+			foreach($options as $key => &$value) {
 				if (!in_array($key, array('validate', 'fieldList', 'callbacks'))) {
 					$extra[$key] = $value;
 				}
@@ -109,46 +150,31 @@ class AppModel extends Model {
 			$options = array_merge($options, compact('validate', 'fieldList', 'callbacks'));
 		}
 
-		$this->behaviorData = $extra;
+		$this->setCallbackableData($extra);
 
 		$method = null;
-		if (isset($extra['callback']) and is_string($extra['callback'])) {
-			$method = sprintf('__beforeSave%s', Inflector::camelize($extra['callback']));
+		if (($callback = $this->getCallbackableData('callback', 'string')) != false) {
+			$method = sprintf('__beforeSave%s', Inflector::camelize($callback));
 		}
 
-		if ($method && method_exists($this, $method)) {
+		if($method && method_exists($this, $method)) {
 			$this->data = $this->{$method}($this->data, $extra);
 		}
 		if (!$this->data) return false;
 		return parent::save($this->data, $options);
 	}
 
-/**
- * Unsets contain key for faster pagination counts
- *
- * @param   array $conditions
- * @param   integer $recursive
- * @param   array $extra
- * @return  integer
- * @access  public
- * @author  Jose Diaz-Gonzalez
- */
-	function paginateCount($conditions = null, $recursive = 0, $extra = array()) {
-		$extra = (array) $extra;
-		$conditions = compact('conditions');
-		if ($recursive != $this->recursive) {
-			$conditions['recursive'] = $recursive;
-		}
-		$extra['contain'] = false;
-		return $this->find('count', array_merge($conditions, $extra));
-	}
 
 /**
- * Updates a particular record without invoking model callbacks
+ * Convenience method to update one record without invoking any callbacks
  *
- * @return  boolean True on success, false on Model::id is not set or failure
- * @access  public
- * @author  Jose Diaz-Gonzalez
+ * @param array $fields Set of fields and values, indexed by fields.
+ *    Fields are treated as SQL snippets, to insert literal values manually escape your data.
+ * @param mixed $conditions Conditions to match, true for all records
+ * @return boolean True on success, false on Model::id is not set or failure
+ * @access public
+ * @author Jose Diaz-Gonzalez
+ * @link http://book.cakephp.org/view/1031/Saving-Your-Data
  **/
 	function update($fields, $conditions = array()) {
 		$conditions = (array) $conditions;
@@ -162,22 +188,19 @@ class AppModel extends Model {
 /**
  * Disables/detaches all behaviors from model
  *
- * @param   mixed $except string or array of behaviors to exclude from detachment
- * @param   boolean $detach If true, detaches the behavior instead of disabling it
- * @return  void
- * @access  public
- * @author  Jose Diaz-Gonzalez
+ * @param mixed $except string or array of behaviors to exclude from detachment
+ * @param boolean $detach If true, detaches the behavior instead of disabling it
+ * @return void
+ * @access public
+ * @author Jose Diaz-Gonzalez
  */
-	function detachAllBehaviors($except = array(), $detach = false) {
-		$except = (array) $except;
-		$behaviors = $this->Behaviors->attached();
+	function disableAllBehaviors($except = array(), $detach = false) {
+		$behaviors = array_diff($this->Behaviors->attached(), (array) $except);
 		foreach ($behaviors as &$behavior) {
-			if (!in_array($behavior, $except)) {
-				if (!$detach) {
-					$this->Behaviors->disable($behavior);
-				} else {
-					$this->Behaviors->detach($behavior);
-				}
+			if ($detach) {
+				$this->Behaviors->detach($behavior);
+			} else {
+				$this->Behaviors->disable($behavior);
 			}
 		}
 	}
@@ -185,13 +208,13 @@ class AppModel extends Model {
 /**
  * Enables all previously disabled attachments
  *
- * @return  void
- * @access  public
- * @author  Jose Diaz-Gonzalez
+ * @return void
+ * @access public
+ * @author Jose Diaz-Gonzalez
  */
 	function enableAllBehaviors() {
 		$behaviors = $this->Behaviors->attached();
-		foreach ($behaviors as &$behavior) {
+		foreach($behaviors as &$behavior) {
 			if (!$this->Behaviors->enabled($behavior)) {
 				$this->Behaviors->enable($behavior);
 			}
@@ -209,5 +232,156 @@ class AppModel extends Model {
 			'contain' => false,
 			'fields' => $fields));
 	}
+
+/**
+ * __findRandom()
+ *
+ * Find a list of records ordered by rank.
+ * Instead of executing a __findList() query to get the list of IDs,
+ * you can pass an array of IDs via the $options['suppliedList']
+ * argument.
+ *
+ * Two queries are executed, first a find('list') to generate a list of primary
+ * keys, and then either a find('all') or find('first') depending on the return
+ * amount specified (default 1).
+ *
+ * Pass find options to each query using the $options['list'] and $options['find']
+ * arguments.
+ *
+ * Specify $options['amount'] as the maximum number of random items that should
+ * be returned.
+ *
+ * If you already have an array of IDs(/primary keys), you can skip the find('list')
+ * query by passing the array as $options['suppliedList'].
+ *
+ * @param $options array of standard and function-specific find options.
+ * @return array
+ * @access public
+ * @author Jamie Nay
+ * @link http://github.com/jamienay/find_random
+ */
+	function __findRandom($options = array()) {
+		if (!isset($options['amount'])) {
+			$amount = 1;
+		} else {
+			$amount = $options['amount'];
+		}
+
+		$findOptions = array();
+		if (isset($options['find'])) {
+			$findOptions = array_merge($findOptions, $options['find']);
+		}
+
+		if (!isset($options['suppliedList'])) {
+			$listOptions = array();
+			if (isset($options['list'])) {
+				$listOptions = array_merge($listOptions, $options['list']);
+			}
+
+			$list = $this->find('list', $listOptions);
+		} else {
+			$list = $options['suppliedList'];
+			$list = array_flip($list);
+		}
+
+		// Just a little failsafe.
+		if (count($list) < 1) {
+			return $list;
+		}
+
+		$originalAmount = null;
+		if ($amount > count($list)) {
+			$originalAmount = $amount;
+			$amount = count($list);
+		}
+
+		$id = array_rand($list, $amount);
+
+		if (is_array($id)) {
+			shuffle($id);
+		}
+
+		if (!isset($findOptions['conditions'])) {
+			$findOptions['conditions'] = array();
+		}
+
+		$findOptions['conditions'][$this->alias.'.'.$this->primaryKey] = $id;
+		if ($amount == 1 && !$originalAmount) {
+			return $this->find('first', $findOptions);
+		} else {
+			return $this->find('all', $findOptions);
+		}
+	}
+
+	function __findLookup($options = array()) {
+		if (!is_array($options)) $options = array('conditions' => array($this->displayField => $options));
+		$options = array_merge(array(
+			'field' => $this->primaryKey,
+			'create' => true,
+			'conditions' => array()
+		), $options);
+
+		if (!empty($options['field'])) {
+			$fieldValue = $this->field($options['field'], $options['condition']);
+		} else {
+			$fieldValue = $this->find('first', $options['conditions']);
+		}
+
+		if ($fieldValue !== false) return $fieldValue;
+		if ($options['create'] === false) return false;
+
+		$this->create($options['conditions']);
+		if (!$this->save()) return false;
+
+		$conditions[$this->primaryKey] = $this->id;
+		if (empty($options['field'])) return $this->read();
+		return $this->field($options['field'], $options['conditions']);
+	}
+
+/**
+ * Checks that a given value is a valid postal code.
+ *
+ * Modified version of Validation::postal - allows for multiple
+ * countries to be specified as an array.
+ *
+ * @param mixed $check Value to check
+ * @param string $regex Regular expression to use
+ * @param mixed $country Countries to use for formatting
+ * @return boolean Success
+ * @access public
+ * @author Jamie Nay
+ * @link http://github.com/jamienay/postal_validation
+ */
+	function postal_multiple($check, $regex = null, $country = null) {
+		// List of regular expressions to use, if a custom one isn't specified.
+		$countryRegs = array(
+			'uk' => '/\\A\\b[A-Z]{1,2}[0-9][A-Z0-9]? [0-9][ABD-HJLNP-UW-Z]{2}\\b\\z/i',
+			'ca' => '/\\A\\b[ABCEGHJKLMNPRSTVXY][0-9][A-Z][ ]?[0-9][A-Z][0-9]\\b\\z/i',
+			'it' => '/^[0-9]{5}$/i',
+			'de' => '/^[0-9]{5}$/i',
+			'be' => '/^[1-9]{1}[0-9]{3}$/i',
+			'us' => '/\\A\\b[0-9]{5}(?:-[0-9]{4})?\\b\\z/i',
+			'default' => '/\\A\\b[0-9]{5}(?:-[0-9]{4})?\\b\\z/i' // Same as US.
+		);
+
+		$value = array_values($check);
+		$value = $value[0];
+		if ($regex) {
+			return preg_match($regex, $value);
+		} else if (!is_array($country)) {
+			return preg_match($countryRegs[$country], $value);
+		}
+
+		foreach ($country as $check) {
+			if (!isset($countryRegs[$check]) && preg_match($countryRegs['default'], $value)) {
+				return true;
+			} else if (preg_match($countryRegs[$check], $value)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
 }
 ?>
--- models/behaviors/callbackable.php
+++ models/behaviors/callbackable.php
@@ -2,53 +2,46 @@
 class CallbackableBehavior extends ModelBehavior {
 
 /**
- * Contains configuration settings for use with individual model objects.
- * Individual model settings should be stored as an associative array, 
- * keyed off of the model name.
+ * Data that is available within the behavior
  *
- * @var array
- * @access public
- * @see Model::$alias
- */
-	var $settings = array();
+ * @var string
+ * @access private
+ **/
+	var $__data = null;
 
 /**
- * Initiate Callbackable Behavior
+ * Convenience function to set extra data for use within callbacks
  *
- * @param object $model
- * @param array $config
  * @return void
+ * @author Jose Diaz-Gonzalez
  * @access public
- */
-	function setup(&$model, $config = array()) {
-
-	}
-
-	/* -- All possible behavior callbacks have been stubbed out. Remove those you do not need. -- */
-
-/**
- * Before find callback
- *
- * @param object $model Model using this behavior
- * @param array $queryData Data used to execute this query, i.e. conditions, order, etc.
- * @return boolean True if the operation should continue, false if it should abort
- * @access public
- */
-	function beforeFind(&$model, $query) { 
-		return true;
+ **/
+	function setCallbackableData(&$model, $data = null) {
+		$this->__data = $data;
+		return $this->__data;
 	}
 
 /**
- * After find callback. Can be used to modify any results returned by find and findAll.
+ * Convenience function to get extra data for use within callbacks
  *
- * @param object $model Model using this behavior
- * @param mixed $results The results of the find operation
- * @param boolean $primary Whether this model is being queried directly (vs. being queried as an association)
- * @return mixed Result of the find operation
+ * Can also optionally check for the type of the returned data and return null
+ * if the type does not match
+ *
+ * @param string $key Optional Key potentially pointing to some data
+ * @param string $type Optional Enforces a type constraint on returned data
+ * @return mixed contents of the key if isset, null otherwise.
+ * @author Jose Diaz-Gonzalez
  * @access public
- */
-	function afterFind(&$model, $results, $primary) { 
-		return $results;
+ **/
+	function getCallbackableData(&$model, $key = null, $type = null) {
+		if (!$key) return $this->__data;
+
+		if (empty($this->__data[$key])) {
+			if (!$type) return $this->__data[$key];
+
+			if (gettype($this->__data[$key]) === $type) return $this->__data[$key];
+		}
+		return null;
 	}
 
 /**
@@ -60,11 +53,11 @@ class CallbackableBehavior extends ModelBehavior {
  */
 	function beforeValidate(&$model) {
 		$method = null;
-		if (isset($model->behaviorData['callback']) and is_string($model->behaviorData['callback'])) {
-			$method = sprintf('__beforeValidate%s', Inflector::camelize($model->behaviorData['callback']));
+		if (($callback = $this->getCallbackableData($model, 'callback', 'string')) != false) {
+			$method = sprintf('__beforeValidate%s', Inflector::camelize($callback));
 		}
 
-		if ($method && method_exists($model, $method)) {
+		if($method && method_exists($model, $method)) {
 			return $model->{$method}($model);
 		}
 		return true;
@@ -80,11 +73,11 @@ class CallbackableBehavior extends ModelBehavior {
  */
 	function afterSave(&$model, $created) {
 		$method = null;
-		if (isset($model->behaviorData['callback']) and is_string($model->behaviorData['callback'])) {
-			$method = sprintf('__afterSave%s', Inflector::camelize($model->behaviorData['callback']));
+		if (($callback = $this->getCallbackableData($model, 'callback', 'string')) != false) {
+			$method = sprintf('__afterSave%s', Inflector::camelize($callback));
 		}
 
-		if ($method && method_exists($model, $method)) {
+		if($method && method_exists($model, $method)) {
 			return $model->{$method}($data);
 		}
 		return true;
@@ -100,11 +93,11 @@ class CallbackableBehavior extends ModelBehavior {
  */
 	function beforeDelete(&$model, $cascade = true) {
 		$method = null;
-		if (isset($model->behaviorData['callback']) and is_string($model->behaviorData['callback'])) {
-			$method = sprintf('__beforeDelete%s', Inflector::camelize($model->behaviorData['callback']));
+		if (($callback = $this->getCallbackableData($model, 'callback', 'string')) != false) {
+			$method = sprintf('__beforeDelete%s', Inflector::camelize($callback));
 		}
 
-		if ($method && method_exists($model, $method)) {
+		if($method && method_exists($model, $method)) {
 			return $model->{$method}($model, $cascade);
 		}
 		return true;
@@ -118,11 +111,11 @@ class CallbackableBehavior extends ModelBehavior {
  */
 	function afterDelete(&$model) {
 		$method = null;
-		if (isset($model->behaviorData['callback']) and is_string($model->behaviorData['callback'])) {
-			$method = sprintf('__afterDelete%s', Inflector::camelize($model->behaviorData['callback']));
+		if (($callback = $this->getCallbackableData($model, 'callback', 'string')) != false) {
+			$method = sprintf('__afterDelete%s', Inflector::camelize($callback));
 		}
 
-		if ($method && method_exists($model, $method)) {
+		if($method && method_exists($model, $method)) {
 			$model->{$method}($model);
 		}
 	}
@@ -136,11 +129,11 @@ class CallbackableBehavior extends ModelBehavior {
  */
 	function onError(&$model, $error) {
 		$method = null;
-		if (isset($model->behaviorData['callback']) and is_string($model->behaviorData['callback'])) {
-			$method = sprintf('__onError%s', Inflector::camelize($model->behaviorData['callback']));
+		if (($callback = $this->getCallbackableData($model,'callback', 'string')) != false) {
+			$method = sprintf('__onError%s', Inflector::camelize($callback));
 		}
 
-		if ($method && method_exists($model, $method)) {
+		if($method && method_exists($model, $method)) {
 			$model->{$method}($model, $error);
 		}
 	}
