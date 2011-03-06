6c9e02696f3d95d0556d24179f92cd2e8eb9fdb6
Jose Diaz-Gonzalez <myaccounts@savant.be>
2011-03-06 17:57:10 -0500

    added cakepackages_god.rb and workers.god for background processes

---
+++ config/cakepackages_god.rb
@@ -0,0 +1,39 @@
+class CakepackagesGod
+  def self.queue_workers(queue, number_of_workers, uid = 'deploy', gid = 'deploy')
+    number_of_workers.times do |i|
+      God.watch do |w|
+        w.name          = "jobs:worker:#{queue}:#{i}"
+        w.group         = "workers"
+        w.interval      = 10.minutes
+        w.log           = "#{ROOT}/shared/log/#{queue}.#{i}.log"
+        w.dir           = "#{ROOT}/public"
+        w.start         = "../cake/console/cake -app #{ROOT}/public worker -queue #{queue} -count 100"
+        w.start_grace   = 10.seconds
+        w.restart_grace = 10.seconds
+        w.uid           = uid
+        w.gid           = gid
+
+        w.behavior(:clean_pid_file)
+
+        w.start_if do |start|
+          start.condition(:process_running) do |c|
+            c.interval = 5.seconds
+            c.running = false
+          end
+        end
+
+        w.restart_if do |restart|
+          restart.condition(:memory_usage) do |c|
+            c.above = 200.megabytes
+            c.times = [3, 5] # 3 out of 5 intervals
+          end
+
+          restart.condition(:cpu_usage) do |c|
+            c.above = 50.percent
+            c.times = 5
+          end
+        end
+      end
+    end
+  end
+end
---
+++ config/workers.god
@@ -0,0 +1,13 @@
+ROOT = "/apps/production/cakepackages.com/default"
+require File.expand_path(File.dirname(__FILE__) + '/cakepackages_god.rb')
+God.pid_file_directory = "/apps/pids"
+God.log_level = :error
+
+%w(default quick).each do |queue|
+  CakepackagesGod.queue_workers(queue, 1)
+end
+
+# run as root
+%w(root).each do |queue|
+  CakepackagesGod.queue_workers(queue, 1, 'root', 'root')
+end
