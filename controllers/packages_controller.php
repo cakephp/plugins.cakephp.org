7e5d4742f58687346c42459a098a2706d115847e
Jose Diaz-Gonzalez <myaccounts@savant.be>
2011-03-19 15:01:19 -0400

    whitespace changes and removing packages/add

--- controllers/packages_controller.php
+++ controllers/packages_controller.php
@@ -1,140 +1,127 @@
 <?php
 class PackagesController extends AppController {
-	var $name = 'Packages';
-	var $components = array('Searchable.Search');
-	var $helpers = array('Searchable.Searchable');
+    var $name = 'Packages';
+    var $components = array('Searchable.Search');
+    var $helpers = array('Searchable.Searchable');
 
-	function home() {
-		$latest = $this->Package->find('latest');
-		$random = $this->Package->find('random');
-		$this->set(compact('latest', 'random'));
-	}
+    function home() {
+        $latest = $this->Package->find('latest');
+        $random = $this->Package->find('random');
+        $this->set(compact('latest', 'random'));
+    }
 
-	function latest() {
-		$this->paginate = array('latest');
-		$this->set('packages', $this->paginate());
-		$this->render('index');
-	}
+    function latest() {
+        $this->paginate = array('latest');
+        $this->set('packages', $this->paginate());
+        $this->render('index');
+    }
 
-	function index($search = null) {
-		$this->paginate = array(
-			'index',
-			'paginateType' => $search
-		);
+    function index($search = null) {
+        $this->paginate = array(
+            'index',
+            'paginateType' => $search
+        );
 
-		$packages = $this->paginate();
+        $packages = $this->paginate();
 
-		$this->set(compact('packages', 'search'));
-		$this->_seoForAction('index');
-	}
+        $this->set(compact('packages', 'search'));
+        $this->_seoForAction('index');
+    }
 
-	function filter() {
-		$search = Inflector::singularize($this->params['by']);
-		$this->paginate = array(
-			'index',
-			'paginateType' => $search,
-		);
+    function filter() {
+        $search = Inflector::singularize($this->params['by']);
+        $this->paginate = array(
+            'index',
+            'paginateType' => $search,
+        );
 
-		$packages = $this->paginate();
+        $packages = $this->paginate();
 
-		$this->set(compact('packages', 'search'));
-		$this->_seoForAction('filter', $search);
-		$this->render('index');
-	}
+        $this->set(compact('packages', 'search'));
+        $this->_seoForAction('filter', $search);
+        $this->render('index');
+    }
 
-	function search($search = null) {
-		// Redirect with search data in the URL in pretty format
-		$this->Search->redirectUnlessGet();
+    function search($search = null) {
+        // Redirect with search data in the URL in pretty format
+        $this->Search->redirectUnlessGet();
 
-		// Get Pagination results
-		$this->loadModel('Searchable.SearchIndex');
-		$packages = $this->Search->paginate($search);
+        // Get Pagination results
+        $this->loadModel('Searchable.SearchIndex');
+        $packages = $this->Search->paginate($search);
 
-		$this->set(compact('packages', 'search'));
-		$this->_seoForAction('search', $search);
-		$this->render('index');
-	}
+        $this->set(compact('packages', 'search'));
+        $this->_seoForAction('search', $search);
+        $this->render('index');
+    }
 
-	function view($maintainer = null, $package = null) {
-		try {
-			$this->set('package', $this->Package->find('view', array(
-				'maintainer' => $maintainer,
-				'package' => $package,
-			)));
-		} catch (Exception $e) {
-			$this->flashAndRedirect($e->getMessage());
-		}
-	}
+    function view($maintainer = null, $package = null) {
+        try {
+            $this->set('package', $this->Package->find('view', array(
+                'maintainer' => $maintainer,
+                'package' => $package,
+            )));
+        } catch (Exception $e) {
+            $this->flashAndRedirect($e->getMessage());
+        }
+    }
 
-	function add() {
-		if (!empty($this->data)) {
-			$this->Package->create();
-			if ($this->Package->save($this->data)) {
-				$this->flashAndRedirect(__('The package has been added', true));
-			} else {
-				$this->Session->setFlash(__('The package could not be saved. Please, try again.', true));
-			}
-		}
+    function edit($id = null) {
+        if (!$id && empty($this->data)) {
+            $this->flashAndRedirect(__('Invalid package', true));
+        }
+        if (!empty($this->data)) {
+            if ($this->Package->save($this->data)) {
+                $this->flashAndRedirect(__('The package has been saved', true));
+            } else {
+                $this->Session->setFlash(__('The package could not be saved. Please, try again.', true));
+            }
+        }
 
-		$this->set('maintainers', $this->Package->Maintainer->find('list'));
-	}
+        if (empty($this->data)) {
+            try {
+                $this->data = $this->Package->find('edit', $id);
+            } catch (Exception $e) {
+                $this->flashAndRedirect($e->getMessage());
+            }
+            $this->redirectUnless($this->data);
+        }
 
-	function edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->flashAndRedirect(__('Invalid package', true));
-		}
-		if (!empty($this->data)) {
-			if ($this->Package->save($this->data)) {
-				$this->flashAndRedirect(__('The package has been saved', true));
-			} else {
-				$this->Session->setFlash(__('The package could not be saved. Please, try again.', true));
-			}
-		}
+        $this->set('maintainers', $this->Package->Maintainer->find('list'));
+    }
 
-		if (empty($this->data)) {
-			try {
-				$this->data = $this->Package->find('edit', $id);
-			} catch (Exception $e) {
-				$this->flashAndRedirect($e->getMessage());
-			}
-			$this->redirectUnless($this->data);
-		}
+    function delete($id = null) {
+        $this->redirectUnless($id);
 
-		$this->set('maintainers', $this->Package->Maintainer->find('list'));
-	}
+        if ($this->Package->delete($id)) {
+            $this->Session->setFlash(sprintf(__('%s deleted', true), 'Package'));
+        } else {
+            $this->Session->setFlash(sprintf(__('%s was not deleted', true), 'Package'));
+        }
 
-	function delete($id = null) {
-		$this->redirectUnless($id);
+        $this->redirect(array('action' => 'index'));
+    }
 
-		if ($this->Package->delete($id)) {
-			$this->Session->setFlash(sprintf(__('%s deleted', true), 'Package'));
-		} else {
-			$this->Session->setFlash(sprintf(__('%s was not deleted', true), 'Package'));
-		}
+    function autocomplete() {
+        $term = (isset($this->params['url']['term'])) ? $this->params['url']['term'] : '';
+        $this->set('results', $this->Package->find('autocomplete', array('term' => $term)));
+        $this->layout = 'ajax';
+        Configure::write('debug', 0);
+    }
 
-		$this->redirect(array('action' => 'index'));
-	}
-
-	function autocomplete() {
-		$term = (isset($this->params['url']['term'])) ? $this->params['url']['term'] : '';
-		$this->set('results', $this->Package->find('autocomplete', array('term' => $term)));
-		$this->layout = 'ajax';
-		Configure::write('debug', 0);
-	}
-
-	function _seoForAction($type = 'index', $extra = null) {
-		if ($type == 'index') {
-			$h2_for_layout = $title_for_layout = 'Browse Packages';
-		}
-		else if ($type == 'filter') {
-			$h2_for_layout = sprintf('Browse Packages containing %ss', $extra);
-			$title_for_layout = $h2_for_layout;
-		}
-		else if ($type == 'search') {
-			$h2_for_layout = sprintf('Search Results for %s', $extra);
-			$title_for_layout = $h2_for_layout;
-		}
-		$this->set(compact('h2_for_layout', 'title_for_layout'));
-	}
+    function _seoForAction($type = 'index', $extra = null) {
+        if ($type == 'index') {
+            $h2_for_layout = $title_for_layout = 'Browse Packages';
+        }
+        else if ($type == 'filter') {
+            $h2_for_layout = sprintf('Browse Packages containing %ss', $extra);
+            $title_for_layout = $h2_for_layout;
+        }
+        else if ($type == 'search') {
+            $h2_for_layout = sprintf('Search Results for %s', $extra);
+            $title_for_layout = $h2_for_layout;
+        }
+        $this->set(compact('h2_for_layout', 'title_for_layout'));
+    }
 
 }
