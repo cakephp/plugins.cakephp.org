881aef739229857211d72619e850f114d75774fe
Jose Diaz-Gonzalez <myaccounts@savant.be>
2011-04-21 22:48:21 -0400

    added UrlCacheAppHelper to application

--- app_helper.php
+++ app_helper.php
@@ -1,5 +1,6 @@
 <?php
-class AppHelper extends Helper {
+App::import('Vendor', 'UrlCache.url_cache_app_helper');
+class AppHelper extends UrlCacheAppHelper {
 	var $view = null;
 
 	function for_layout($content, $name) {
@@ -7,21 +8,17 @@ class AppHelper extends Helper {
 		if (!$this->view) {
 			$this->view = ClassRegistry::getObject('view');
 		}
-		echo "{$content}";
+		echo $content;
 		$this->view->set("{$name}_for_layout", ob_get_clean());
 	}
 
 	function h2($contents, $alternate = null) {
-		ob_start();
-		if ((empty($contents) || $contents == '' || $contents == ' ') && isset($alternate)) {
+		$alt = trim($contents);
+		if ((strlen($alt) === 0) && isset($alternate)) {
 			$contents = $alternate;
 		}
-		if (!$this->view) {
-			$this->view = ClassRegistry::getObject('view');
-		}
-		echo "<h2>{$contents}</h2>";
-		$this->view->set('title_for_layout', "{$contents} |");
-		$this->view->set("h2_for_layout", ob_get_clean());
+
+		$this->for_layout("<h2>{$contents}</h2> |", 'title');
+		$this->for_layout("<h2>{$contents}</h2>", 'h2');
 	}
 }
-?>
