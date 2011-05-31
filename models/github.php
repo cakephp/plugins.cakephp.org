d01643ddfc62601da9d2990b2c308ae57b26b214
Jose Diaz-Gonzalez <myaccounts@savant.be>
2011-05-30 20:40:47 -0400

    change find to get calls for github model

--- models/github.php
+++ models/github.php
@@ -11,31 +11,31 @@ class Github extends AppModel {
 		$this->base_uri = "https://github.com/api/v2/xml";
 	}
 
-	function __findUserShow($username = null) {
+	function _getUserShow($username = null) {
 		if (!$username) return false;
 
 		return $this->cached_xml_get("/user/show/{$username}");
 	}
 
-	function __findUserShowFollowing($username = null) {
+	function _getUserShowFollowing($username = null) {
 		if (!$username) return false;
 
 		return $this->cached_xml_get("/user/show/{$username}/following");
 	}
 
-	function __findUserWatched($username = null) {
+	function _getUserWatched($username = null) {
 		if (!$username) return false;
 
 		return $this->cached_xml_get("/repos/watched/{$username}");
 	}
 
-	function __findUserEmail($username = null) {
+	function _getUserEmail($username = null) {
 		if (!$username) return false;
 
 		return $this->cached_xml_get("/user/email/{$username}");
 	}
 
-	function __findIssuesSearch($params = array()) {
+	function _getIssuesSearch($params = array()) {
 		foreach (array('username', 'repo', 'term') as $param) {
 			if (empty($params[$param])) return false;
 		}
@@ -45,14 +45,14 @@ class Github extends AppModel {
 		return $this->cached_xml_get($request);
 	}
 
-	function __findIssuesList($params = array()) {
+	function _getIssuesList($params = array()) {
 		if (empty($params['username']) || empty($params['repo'])) return false;
 		$params['state'] = (empty($params['state'])) ? 'open' : $params['state'];
 
 		return $this->cached_xml_get("/issues/list/{$params['username']}/{$params['repo']}/{$params['state']}");
 	}
 
-	function __findIssuesShow($params = array()) {
+	function _getIssuesShow($params = array()) {
 		foreach (array('username', 'repo', 'number') as $param) {
 			if (empty($params[$param])) return false;
 		}
@@ -61,7 +61,7 @@ class Github extends AppModel {
 		return $this->cached_xml_get("/issues/show/{$params['username']}/{$params['repo']}/{$params['number']}");
 	}
 
-	function __findIssuesComments($params = array()) {
+	function _getIssuesComments($params = array()) {
 		foreach (array('username', 'repo', 'number') as $param) {
 			if (empty($params[$param])) return false;
 		}
@@ -70,76 +70,76 @@ class Github extends AppModel {
 		return $this->cached_xml_get("/issues/comments/{$params['username']}/{$params['repo']}/{$params['number']}");
 	}
 
-	function __findIssuesLabels($params = array()) {
+	function _getIssuesLabels($params = array()) {
 		if (empty($params['username']) || empty($params['repo'])) return false;
 		$params['state'] = (empty($params['state'])) ? 'open' : $params['state'];
 
 		return $this->cached_xml_get("/issues/labels/{$params['username']}/{$params['repo']}");
 	}
 
-	function __findReposWatched($username = null) {
+	function _getReposWatched($username = null) {
 		if (!$username) return false;
 
 		return $this->cached_xml_get("/repos/watched/{$username}");
 	}
 
-	function __findReposSearch($query = null) {
+	function _getReposSearch($query = null) {
 		if (!$query) return false;
 
 		return $this->cached_xml_get("/repos/search/", str_replace(' ', '+', $query));
 	}
 
-	function __findReposShowSingle($params = array()) {
+	function _getReposShowSingle($params = array()) {
 		if (empty($params['username']) || empty($params['repo'])) return false;
 
 		return $this->cached_xml_get("/repos/show/{$params['username']}/{$params['repo']}");
 	}
 
-	function __findReposShow($username = null) {
+	function _getReposShow($username = null) {
 		if (!$username) return false;
 
 		return $this->cached_xml_get("/repos/show/{$username}");
 	}
 
-	function __findReposShowCollaborators($params = array()) {
+	function _getReposShowCollaborators($params = array()) {
 		if (empty($params['username']) || empty($params['repo'])) return false;
 
 		return $this->cached_xml_get("/repos/show/{$params['username']}/{$params['repo']}/collaborators");
 	}
 
 
-	function __findReposShowContributors($params = array()) {
+	function _getReposShowContributors($params = array()) {
 		if (empty($params['username']) || empty($params['repo'])) return false;
 
 		return $this->cached_xml_get("/repos/show/{$params['username']}/{$params['repo']}/contributors");
 	}
 
-	function __findReposShowNetwork($params = array()) {
+	function _getReposShowNetwork($params = array()) {
 		if (empty($params['username']) || empty($params['repo'])) return false;
 
 		return $this->cached_xml_get("/repos/show/{$params['username']}/{$params['repo']}/network");
 	}
 
-	function __findReposShowLanguages($params = array()) {
+	function _getReposShowLanguages($params = array()) {
 		if (empty($params['username']) || empty($params['repo'])) return false;
 
 		return $this->cached_xml_get("/repos/show/{$params['username']}/{$params['repo']}/languages");
 	}
 
-	function __findReposShowTags($params = array()) {
+	function _getReposShowTags($params = array()) {
 		if (empty($params['username']) || empty($params['repo'])) return false;
 
 		return $this->cached_xml_get("/repos/show/{$params['username']}/{$params['repo']}/tags");
 	}
 
-	function __findCommitsList($params = array()) {
+	function _getCommitsList($params = array()) {
 		if (empty($params['username']) || empty($params['repo'])) return false;
 		$params['branch'] = (empty($params['branch'])) ? 'master' : $params['branch'];
 
 		return $this->cached_xml_get("/commits/list/{$params['username']}/{$params['repo']}/{$params['branch']}");
 	}
 
-	function __findCommitsShowPath($params = array()) {
+	function _getCommitsShowPath($params = array()) {
 		foreach (array('username', 'repo', 'path') as $param) {
 			if (empty($params[$param])) return false;
 		}
@@ -149,7 +149,7 @@ class Github extends AppModel {
 		return $this->cached_xml_get($request);
 	}
 
-	function __findCommitsShowSha($params = array()) {
+	function _getCommitsShowSha($params = array()) {
 		foreach (array('username', 'repo', 'sha') as $param) {
 			if (empty($params[$param])) return false;
 		}
@@ -157,7 +157,7 @@ class Github extends AppModel {
 		return $this->cached_xml_get("/commits/show/{$params['username']}/{$params['repo']}/{$params['sha']}");
 	}
 
-	function __findTreeShow($params = array()) {
+	function _getTreeShow($params = array()) {
 		foreach (array('username', 'repo', 'sha') as $param) {
 			if (empty($params[$param])) return false;
 		}
@@ -165,7 +165,7 @@ class Github extends AppModel {
 		return $this->cached_xml_get("/tree/show/{$params['username']}/{$params['repo']}/{$params['sha']}");
 	}
 
-	function __findBlobShowPath($params = array()) {
+	function _getBlobShowPath($params = array()) {
 		foreach (array('username', 'repo', 'sha', 'path') as $param) {
 			if (empty($params[$param])) return false;
 		}
@@ -174,7 +174,7 @@ class Github extends AppModel {
 		return $this->cached_xml_get($request);
 	}
 
-	function __findBlobShowAll($params = array()) {
+	function _getBlobShowAll($params = array()) {
 		foreach (array('username', 'repo', 'sha') as $param) {
 			if (empty($params[$param])) return false;
 		}
@@ -182,7 +182,7 @@ class Github extends AppModel {
 		return $this->cached_xml_get("/blob/all/{$params['username']}/{$params['repo']}/{$params['sha']}");
 	}
 
-	function __findBlobShow($params = array()) {
+	function _getBlobShow($params = array()) {
 		foreach (array('username', 'repo', 'sha') as $param) {
 			if (empty($params[$param])) return false;
 		}
@@ -190,12 +190,12 @@ class Github extends AppModel {
 		return $this->cached_xml_get("/blob/show/{$params['username']}/{$params['repo']}/{$params['sha']}");
 	}
 
-	function __findNewPackages($username = null) {
+	function _getNewPackages($username = null) {
 		if (!$username) return false;
 		ClassRegistry::init('Maintainer');
 		$Maintainer = &new Maintainer;
 		$existingUser = $Maintainer->find('view', $username);
-		$repoList = $this->find('repos_show', $username);
+		$repoList = $this->get('repos_show', $username);
 
 		$repos = $Maintainer->Package->find('list', array(
 			'conditions' => array(
@@ -215,8 +215,8 @@ class Github extends AppModel {
 		}
 	}
 
-	function __findUnlisted($username = 'josegonzalez') {
-		$following = $this->find('users_show_following', 'josegonzalez');
+	function _getUnlisted($username = 'josegonzalez') {
+		$following = $this->get('users_show_following', 'josegonzalez');
 		ClassRegistry::init('Maintainer');
 		$maintainer = &new Maintainer;
 		$maintainers = $maintainer->find('list', array('fields' => array('username')));
@@ -266,13 +266,13 @@ class Github extends AppModel {
 		return $this->enqueue(new NewMaintainerJob($username, $name));
 	}
 
-	function __findRelatedRepositories($maintainers = array()) {
+	function _getRelatedRepositories($maintainers = array()) {
 		if (!$maintainers) return false;
 
 		$Package = ClassRegistry::init('Package');
 
 		foreach ($maintainers as $i => $maintainer) {
-			$repos = $this->find('repos_show', $maintainer['Maintainer']['username']);
+			$repos = $this->get('repos_show', $maintainer['Maintainer']['username']);
 			if (!empty($repos['Repositories']['Repository'])) {
 				$packages = $Package->find('listformaintainer', $maintainer['Maintainer']['id']);
 				if (!empty($repos['Repositories']['Repository']['name'])) {
