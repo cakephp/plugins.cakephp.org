ed1a086ddf3d356800e94c955275cc95cad67600
Jose Diaz-Gonzalez <myaccounts@savant.be>
2011-12-06 23:13:48 -0500

    remove uniform reference

--- webroot/js/style.js
+++ webroot/js/style.js
@@ -1,6 +1,5 @@
 jQuery.noConflict();
 jQuery(function() {
-	jQuery("select, input:checkbox, input:radio, input:file, .contents input").uniform();
 	jQuery('.clone-link').click(function(e) {
 		jQuery('.clone-link').focus();
 		jQuery('.clone-link').select();
