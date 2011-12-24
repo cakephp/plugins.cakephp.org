eb4c2c9bf13bdf5359aefd9c35c46922382f74be
Jose Diaz-Gonzalez <myaccounts@savant.be>
2011-12-24 18:08:29 -0500

    fake "responsive" design

--- webroot/js/style.js
+++ webroot/js/style.js
@@ -1,6 +1,32 @@
-$(function() {
+(function($){
 	$('#clone').click(function(e) {
 		$('#clone').focus();
 		$('#clone').select();
 	});
-});
+})(jQuery);
+
+(function($){
+  var detect_width = function() {
+    var current_width = $(window).width();
+    if (current_width > 980) {
+      jQuery('body').removeClass("minimal");
+      jQuery('body').removeClass("mobile");
+    } else if (current_width > 610) {
+      jQuery('body').addClass("minimal");
+      jQuery('body').removeClass("mobile");
+    } else {
+      jQuery('body').removeClass("minimal");
+      jQuery('body').addClass("mobile");
+    }
+  };
+
+  //detect the width on page load
+  $(document).ready(function() {
+    detect_width();
+  });
+
+  $(window).resize(function() {
+    detect_width();
+  });
+
+})(jQuery);
