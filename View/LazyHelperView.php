f3b2fcb0f7a772368193e8b48d6342942980227e
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-12-16 00:36:48 -0500

    Removed unnecessary _paths override for theme support

--- View/LazyHelperView.php
+++ View/LazyHelperView.php
@@ -106,32 +106,4 @@ class LazyHelperView extends View {
 		return parent::__get($helperName);
 	}
 
-/**
- * Return all possible paths to find view files in order
- *
- * @param string $plugin The name of the plugin views are being found for.
- * @param boolean $cached Set to true to force dir scan.
- * @return array paths
- * @todo Make theme path building respect $cached parameter.
- */
-	protected function _paths($plugin = null, $cached = true) {
-		$paths = parent::_paths($plugin, $cached);
-		$themePaths = array();
-
-		if (!empty($this->theme)) {
-			$count = count($paths);
-			for ($i = 0; $i < $count; $i++) {
-				if (strpos($paths[$i], DS . 'Plugin' . DS) === false
-					&& strpos($paths[$i], DS . 'Cake' . DS . 'View') === false) {
-						if ($plugin) {
-							$themePaths[] = $paths[$i] . 'Themed'. DS . $this->theme . DS . 'Plugin' . DS . $plugin . DS;
-						}
-						$themePaths[] = $paths[$i] . 'Themed'. DS . $this->theme . DS;
-					}
-			}
-			$paths = array_merge($themePaths, $paths);
-		}
-		return $paths;
-	}
-
 }
