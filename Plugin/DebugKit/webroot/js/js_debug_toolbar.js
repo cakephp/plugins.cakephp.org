97c9cf4cd9dba400862cb800932cc6fec32b18f7
Jose Diaz-Gonzalez <myaccounts@savant.be>
2014-08-30 19:19:32 -0400

    Upgrade DebugKit plugin

--- Plugin/DebugKit/webroot/js/js_debug_toolbar.js
+++ Plugin/DebugKit/webroot/js/js_debug_toolbar.js
@@ -57,7 +57,7 @@ var DEBUGKIT = function () {
 	}
 
 	// Look for existing jQuery that matches the requirements.
-	if (window.jQuery && versionWithin(jQuery.fn.jquery, "1.8", "2.0")) {
+	if (window.jQuery && versionWithin(jQuery.fn.jquery, "1.8", "2.1")) {
 		DEBUGKIT.$ = window.jQuery;
 	} else {
 		// sync load the file. Using document.write() does not block
@@ -97,7 +97,7 @@ DEBUGKIT.sqlLog = function () {
 
 	return {
 		init : function () {
-			var sqlPanel = $('#sqllog-tab');
+			var sqlPanel = $('#sql_log-tab');
 			var buttons = sqlPanel.find('input');
 
 			// Button handling code for explain links.
