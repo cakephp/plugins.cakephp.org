54af37bc78f0e1df153d542179e0f0af7e8ca985
Jose Diaz-Gonzalez <myaccounts@savant.be>
2014-10-12 20:27:14 -0400

    Log the version for debugging

--- Console/Command/UpdatePackageJob.php
+++ Console/Command/UpdatePackageJob.php
@@ -6,6 +6,7 @@ class UpdatePackageJob extends AppShell {
     public $uses = array('Package');
 
     public function work() {
+    	$this->out(sprintf('CAKE_VERSION: %s', CAKE_VERSION));
         $id = $this->args[0];
         $this->out(sprintf('Retrieving package %d', $id));
         $package = $this->Package->find('first', array(
