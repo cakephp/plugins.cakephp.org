4275f6bc09c7274d4427c078374aae08152470af
Jose Diaz-Gonzalez <myaccounts@savant.be>
2015-02-28 15:56:44 -0500

    PSR-2

--- Config/database.php
+++ Config/database.php
@@ -1,70 +1,70 @@
 <?php
-class DATABASE_CONFIG {
+class DATABASE_CONFIG
+{
+    public $default = array(
+        'datasource' => 'Database/Mysql',
+        'persistent' => false,
+        'host' => 'localhost',
+        'login' => 'user',
+        'password' => 'password',
+        'database' => 'cakepackages',
+        'prefix' => '',
+        'encoding' => 'utf8',
+    );
 
-	public $default = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'cakepackages',
-		'prefix' => '',
-		'encoding' => 'utf8',
-	);
+    public $test = array(
+        'database' => 'test',
+    );
 
-	public $test = array(
-		'database' => 'test',
-	);
+    public $development = array(
+        'datasource' => 'Database/MysqlLog',
+        'login' => 'user',
+        'password' => 'password',
+        'database' => 'cakepackages',
+    );
 
-	public $development = array(
-		'datasource' => 'Database/MysqlLog',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'cakepackages',
-	);
+    public $staging = array(
+        'login' => 'cakepackages_sta',
+        'password' => 'cakepackages_sta',
+        'database' => 'cakepackages_staging',
+    );
 
-	public $staging = array(
-		'login' => 'cakepackages_sta',
-		'password' => 'cakepackages_sta',
-		'database' => 'cakepackages_staging',
-	);
+    public $production = array(
+        'login' => 'cakepackages',
+        'password' => 'cakepackages',
+        'database' => 'cakepackages',
+    );
 
-	public $production = array(
-		'login' => 'cakepackages',
-		'password' => 'cakepackages',
-		'database' => 'cakepackages',
-	);
+    public $test_cakeusers = array(
+    );
 
-	public $test_cakeusers = array(
-	);
+    public $development_cakeusers = array(
+    );
 
-	public $development_cakeusers = array(
-	);
+    public $staging_cakeusers = array(
+    );
 
-	public $staging_cakeusers = array(
-	);
+    public $production_cakeusers = array(
+        'database' => 'cakeusers',
+    );
 
-	public $production_cakeusers = array(
-		'database' => 'cakeusers',
-	);
+    public $github = array(
+        'datasource' => 'GithubSource',
+        'token' => null,
+    );
 
-	public $github = array(
-		'datasource' => 'GithubSource',
-		'token' => null,
-	);
+    protected $_skip = array(
+        '_skip', 'default', 'github', '_environments',
+        'test_cakeusers', 'development_cakeusers',
+        'staging_cakeusers', 'production_cakeusers',
+    );
 
-	protected $_skip = array(
-		'_skip', 'default', 'github', '_environments',
-		'test_cakeusers', 'development_cakeusers',
-		'staging_cakeusers', 'production_cakeusers',
-	);
-
-	protected $_environments = array(
-		'development' => array('development_cakeusers'),
-		'staging' => array('staging_cakeusers'),
-		'production' => array('production_cakeusers'),
-		'test' => array('test_cakeusers'),
-	);
+    protected $_environments = array(
+        'development' => array('development_cakeusers'),
+        'staging' => array('staging_cakeusers'),
+        'production' => array('production_cakeusers'),
+        'test' => array('test_cakeusers'),
+    );
 
 /**
  * Generates a connection based on the current environment
@@ -73,34 +73,34 @@ class DATABASE_CONFIG {
  *
  * @todo Support multiple in-environment connections
  */
-	public function __construct() {
-		// once Environment has decided where we at, it will write the name into Configure.
-		if ($environment = Configure::read('Environment.name')) {
+    public function __construct()
+    {
+        // once Environment has decided where we at, it will write the name into Configure.
+        if ($environment = Configure::read('Environment.name')) {
 
-			// Require that the environment have a database configuration
-			if (!isset($this->{$environment})) {
-				throw new RuntimeException(sprintf('Missing Database Configuration %s', $environment));
-			}
+            // Require that the environment have a database configuration
+            if (!isset($this->{$environment})) {
+                throw new RuntimeException(sprintf('Missing Database Configuration %s', $environment));
+            }
 
-			// Merge environment into defaults
-			$this->default = array_merge($this->default, $this->{$environment});
+            // Merge environment into defaults
+            $this->default = array_merge($this->default, $this->{$environment});
 
-			// Merge environment with the environment-specific configurations
-			if (isset($this->_environments[$environment])) {
-				foreach ($this->_environments[$environment] as $name) {
-					$this->$name = array_merge($this->default, $this->$name);
-				}
-			}
-		}
+            // Merge environment with the environment-specific configurations
+            if (isset($this->_environments[$environment])) {
+                foreach ($this->_environments[$environment] as $name) {
+                    $this->$name = array_merge($this->default, $this->$name);
+                }
+            }
+        }
 
-		// if everything above went smooth, $this->default now has the correct login info.
-		foreach (get_object_vars($this) as $name => $config) {
-			if (in_array($name, $this->_skip)) {
-				continue;
-			}
+        // if everything above went smooth, $this->default now has the correct login info.
+        foreach (get_object_vars($this) as $name => $config) {
+            if (in_array($name, $this->_skip)) {
+                continue;
+            }
 
-			$this->$name = array_merge($this->default, $config);
-		}
-	}
-
+            $this->$name = array_merge($this->default, $config);
+        }
+    }
 }
