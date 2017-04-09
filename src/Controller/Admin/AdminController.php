9591d0caf4b471827df50b7c8a884cc518ec74a8
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2017-04-09 14:31:37 -0400

    feat: initial admin controller

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Controller\Admin;
+
+use App\Controller\AppController;
+
+class AdminController extends AppController
+{
+    public function index()
+    {
+    }
+
+    public function suggested_packages()
+    {
+    }
+
+    public function featured_packages()
+    {
+    }
+
+    public function unversioned_packages()
+    {
+    }
+
+    public function uncategorized_packages()
+    {
+    }
+}
