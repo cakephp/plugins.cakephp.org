28f91a8f8f39e00df0fe5e1c3271e77f0d9a15d4
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2017-06-27 20:17:56 -0600

    feat: log when a job is started

--- src/Shell/QueuesadillaShell.php
+++ src/Shell/QueuesadillaShell.php
@@ -10,6 +10,15 @@ class QueuesadillaShell extends Shell
     {
         $worker = parent::getWorker($engine, $logger);
 
+        $worker->attachListener('Worker.job.start', function ($event) {
+            $item = $event->data()['job']->item();
+            if (is_array($item['class']) && count($item['class']) == 2) {
+                $this->info(sprintf('Started job: %s', $className = $item['class'][0]));
+            } else {
+                $this->info('Started job: callable');
+            }
+        });
+
         $worker->attachListener('Worker.job.success', function ($event) {
             ConnectionManager::get('default')->disconnect();
         });
