72befbe1632d61976b053d365ac34fe0b03be7a6
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2018-01-20 18:37:47 -0500

    feat: add a warning output for the logtrait

--- src/Traits/LogTrait.php
+++ src/Traits/LogTrait.php
@@ -7,6 +7,18 @@ use Psr\Log\LogLevel;
 trait LogTrait
 {
     /**
+     * Convenience method to write an debug message to Log. See Log::write()
+     * for more information on writing to logs.
+     *
+     * @param mixed $msg Log message.
+     * @param string|array $context Additional log data relevant to this message.
+     * @return bool Success of log write.
+     */
+    protected function debug($msg, $context = [])
+    {
+        return Log::write(LogLevel::DEBUG, $msg, $context);
+    }
+    /**
      * Convenience method to write an error message to Log. See Log::write()
      * for more information on writing to logs.
      *
@@ -20,19 +32,6 @@ trait LogTrait
     }
 
     /**
-     * Convenience method to write an debug message to Log. See Log::write()
-     * for more information on writing to logs.
-     *
-     * @param mixed $msg Log message.
-     * @param string|array $context Additional log data relevant to this message.
-     * @return bool Success of log write.
-     */
-    protected function debug($msg, $context = [])
-    {
-        return Log::write(LogLevel::DEBUG, $msg, $context);
-    }
-
-    /**
      * Convenience method to write an info message to Log. See Log::write()
      * for more information on writing to logs.
      *
@@ -43,5 +42,18 @@ trait LogTrait
     protected function info($msg, $context = [])
     {
         return Log::write(LogLevel::INFO, $msg, $context);
+    }
+
+    /**
+     * Convenience method to write an warning message to Log. See Log::write()
+     * for more information on writing to logs.
+     *
+     * @param mixed $msg Log message.
+     * @param string|array $context Additional log data relevant to this message.
+     * @return bool Success of log write.
+     */
+    protected function warning($msg, $context = [])
+    {
+        return Log::write(LogLevel::WARNING, $msg, $context);
     }
 }
