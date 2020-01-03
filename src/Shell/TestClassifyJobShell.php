2e5e50649671bfdc0b96736def2c93d368c3769c
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2020-01-02 23:36:36 -0500

    refactor: move Job performer logic into separate class

--- src/Shell/TestClassifyJobShell.php
+++ src/Shell/TestClassifyJobShell.php
@@ -1,10 +1,8 @@
 <?php
 namespace App\Shell;
 
+use App\Job\Performer;
 use Cake\Console\Shell;
-use josegonzalez\Queuesadilla\Engine\NullEngine;
-use josegonzalez\Queuesadilla\Job\Base;
-use Psr\Log\NullLogger;
 
 /**
  * TestClassifyJob shell command.
@@ -41,48 +39,9 @@ class TestClassifyJobShell extends Shell
             return false;
         }
 
-        $item = [
-            'id' => '30',
-            'class' => [
-                '\App\Job\ClassifyJob',
-                'perform'
-            ],
-            'args' => [
-                [
-                    'package_id' => $this->args[0],
-                ]
-            ],
-            'queue' => 'default',
-            'options' => [
-                'attempts_delay' => 600
-            ],
-            'attempts' => 0
-        ];
-        $this->perform($item);
-    }
-
-    public function perform($item)
-    {
-        $job = new Base($item, new NullEngine(new NullLogger, []));
-
-        if (!is_callable($item['class'])) {
-            return false;
-        }
-
-        $success = false;
-        if (is_array($item['class']) && count($item['class']) == 2) {
-            $className = $item['class'][0];
-            $methodName = $item['class'][1];
-            $instance = new $className;
-            $success = $instance->$methodName($job);
-        } elseif (is_string($item['class'])) {
-            $success = call_user_func($item['class'], $job);
-        }
-
-        if ($success !== false) {
-            $success = true;
-        }
-
-        return $success;
+        $callable = ['\App\Job\ClassifyJob','perform'];
+        $parameters = ['package_id' => $this->args[0]];
+        $performer = new Performer($callable, $parameters);
+        return $performer->execute();
     }
 }
